f2258cc87d2f20a6513ffa393d0c47a9ca1b0943
NMarko95 <98682412+NMarko95@users.noreply.github.com>
2023-07-25 13:48:43 +0200

    Add files via upload

--- praksa.js
+++ praksa.js
@@ -6,12 +6,10 @@ for (let i = 0; i < 5; i++) {
   board[i] = new Array(5);
 }
 
-const aspectRatioScreen = {
-  widthScale: 16,
-  heightScale: 9,
-};
-
 let currentTime, lastRender;
+
+let historyImages = [],
+  currentHistoryImg;
 
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
@@ -21,7 +19,7 @@ canvas.height = innerHeight;
 
 let tableDim = 5;
 
-const cellWidth = canvas.width / 5;
+const cellWidth = canvas.width;
 const cellHeight = canvas.height / 5;
 
 const symbolHeight = cellHeight / 1.5;
@@ -30,18 +28,18 @@ const symbolWidth = cellWidth / 1.5;
 let numOfSymbols = 11;
 
 c.lineWidth = 5;
-c.strokeStyle = "gold";
+c.strokeStyle = "black";
 
 function drawTable() {
-  for (let i = 0; i < tableDim; i++)
-    c.strokeRect(i * cellWidth, 0, cellWidth, canvas.height);
+  //for (let i = 0; i < tableDim; i++)
+  c.strokeRect(0, 0, cellWidth, canvas.height);
 }
 
 function generateRandomNumber() {
   return Math.floor(Math.random() * numOfSymbols);
 }
 
-let images = [],
+let symbols = [],
   imagesLoaded = false;
 
 function getImages(counter, isSymbols) {
@@ -55,7 +53,7 @@ function getImages(counter, isSymbols) {
     `./images/${isSymbols ? "Symbols" : "Sprites"}/${counter}.png`
   );
   image.onload = () => {
-    if (isSymbols) images.push(image);
+    if (isSymbols) symbols.push(image);
     else sprites.push(image);
     getImages(counter + 1, isSymbols);
   };
@@ -68,23 +66,24 @@ let randomNumber,
   centeredWidth = (cellWidth - symbolWidth) / 2;
 
 function drawImages() {
-  for (let j = 0; j < tableDim; j++) {
-    for (let i = 0; i <= tableDim; i++) {
-      randomNumber = generateRandomNumber();
-      board[j][i] = {
-        y: (i - 1) * cellHeight + centeredHeight,
-        x: j * cellWidth + centeredWidth,
-        img: images[randomNumber],
-      };
-      c.drawImage(
-        images[randomNumber],
-        j * cellWidth + centeredWidth,
-        (i - 1) * cellHeight + centeredHeight,
-        symbolWidth,
-        symbolHeight
-      );
-    }
+  //for (let j = 0; j < tableDim; j++) {
+  for (let i = 0; i <= tableDim; i++) {
+    randomNumber = generateRandomNumber();
+    board[0][i] = {
+      y: (i - 1) * cellHeight + centeredHeight,
+      x: centeredWidth,
+      img: symbols[randomNumber],
+    };
+    c.drawImage(
+      symbols[randomNumber],
+      centeredWidth,
+      //j * cellWidth + centeredWidth,
+      (i - 1) * cellHeight + centeredHeight,
+      symbolWidth,
+      symbolHeight
+    );
   }
+  //}
 }
 
 let currentImg = generateRandomNumber(),
@@ -93,30 +92,48 @@ let currentImg = generateRandomNumber(),
 let animateCounter = 0,
   currentSymbol,
   movementSpeed = canvas.height / 15,
-  lastAnimate = 62,
-  prevFinishedAnimate = 10,
-  spaceBlocked = false,
-  differenceLines;
+  animatesPerSecond = 62,
+  lastAnimate = animatesPerSecond * 3,
+  prevFinishedAnimate = lastAnimate / (tableDim - 1),
+  spaceBlocked = false;
 
-function drawSymbol(currentSymbol, i, j) {
+let stoppedColumns = new Array(tableDim);
+stoppedColumns.fill(false, 0);
+
+function drawSymbol(currentSymbol, j) {
+  /*if (currentSymbol.y >= canvas.height) {
+    currentImg = generateRandomNumber();
+    currentSymbol.img = symbols[currentImg];
+    currentSymbol.y = -cellHeight + centeredHeight;
+  }
+  if (animateCounter >= lastAnimate + (i - 1) * prevFinishedAnimate) {
+    currentSymbol.y = (j - 1) * cellHeight + centeredHeight;
+    board[i][j] = currentSymbol;
+  }
+  c.drawImage(
+    currentSymbol.img,
+    i * cellWidth + centeredWidth,
+    currentSymbol.y,
+    symbolWidth,
+    symbolHeight
+  );
+  currentSymbol.y += movementSpeed;*/
   if (currentSymbol.y >= canvas.height) {
     currentImg = generateRandomNumber();
-    currentSymbol.img = images[currentImg];
+    currentSymbol.img = symbols[currentImg];
     currentSymbol.y = -cellHeight + centeredHeight;
   }
   if (animateCounter + 1 === lastAnimate) {
     currentSymbol.y = (j - 1) * cellHeight + centeredHeight;
-    board[i][j] = currentSymbol;
+    board[0][j] = currentSymbol;
   }
-  if (currentSymbol.img !== undefined) {
-    c.drawImage(
-      currentSymbol.img,
-      i * cellWidth + centeredWidth,
-      currentSymbol.y,
-      symbolWidth,
-      symbolHeight
-    );
-  }
+  c.drawImage(
+    currentSymbol.img,
+    centeredWidth,
+    currentSymbol.y,
+    symbolWidth,
+    symbolHeight
+  );
   currentSymbol.y += movementSpeed;
 }
 
@@ -125,24 +142,41 @@ function animateSymbols() {
     animateCounter = 0;
     cancelAnimationFrame(animateId);
     spaceBlocked = false;
+    stoppedColumns.fill(false, 0);
     checkWin();
   } else {
-    if (animateCounter < lastAnimate)
-      for (let i = 0; i < tableDim; i++) {
-        c.clearRect(i * cellWidth, 0, cellWidth, canvas.height);
-        for (let j = 0; j <= tableDim; j++) {
-          currentSymbol = board[i][j];
-          drawSymbol(currentSymbol, i, j);
-        }
+    if (animateCounter === animatesPerSecond * 2.5)
+      movementSpeed = canvas.height / 60;
+    /*for (let i = 0; i < tableDim; i++) {
+      if (animateCounter >= lastAnimate + (i - 1) * prevFinishedAnimate) {
+        if (!stoppedColumns[i]) {
+          c.clearRect(i * cellWidth, 0, cellWidth, canvas.height);
+          for (let j = 0; j <= tableDim; j++) {
+            currentSymbol = board[i][j];
+            drawSymbol(currentSymbol, i, j);
+          }
+        } else stoppedColumns[i] = true;
+        continue;
       }
-    drawTable();
+      c.clearRect(i * cellWidth, 0, cellWidth, canvas.height);
+      for (let j = 0; j <= tableDim; j++) {
+        currentSymbol = board[i][j];
+        drawSymbol(currentSymbol, i, j);
+      }
+    }*/
+    c.clearRect(0, 0, cellWidth, canvas.height);
+    for (let j = 0; j <= tableDim; j++) {
+      currentSymbol = board[0][j];
+      drawSymbol(currentSymbol, j);
+    }
+    //drawTable();
     animateCounter += 1;
     animateId = requestAnimationFrame(animateSymbols);
   }
 }
 
 let winningSymbols = [],
-  winSymbolCheck = "4",
+  winSymbolCheck = "1",
   parsedWinSymbolCheck = parseInt(winSymbolCheck),
   currentSprite,
   spriteAnimateCounter = 0,
@@ -156,9 +190,13 @@ function animateWinSymbols() {
   }
   if (spriteAnimateCounter >= 20) spriteAnimateCounter = 0;
   winningSymbols.forEach((symbol) => {
-    c.clearRect(symbol.x, symbol.y - movementSpeed, symbolWidth, symbolHeight);
+    c.clearRect(
+      centeredWidth,
+      symbol.y - movementSpeed,
+      symbolWidth,
+      symbolHeight
+    );
     currentSprite = sprites[parsedWinSymbolCheck];
-    c;
     c.drawImage(
       sprites[parsedWinSymbolCheck],
       0,
@@ -181,27 +219,28 @@ function animateWin() {
 }
 
 function checkWin() {
-  getImages(0, false);
+  document.body.style.pointerEvents = "auto";
+  historyImages.push(canvas.toDataURL());
   let symbolNumber, symbolSrcArray, symbolImg;
-  for (let i = 0; i < tableDim; i++) {
-    for (let j = 0; j <= tableDim; j++) {
-      symbolImg = board[i][j].img;
-      symbolSrcArray = symbolImg.src.split("/");
-      symbolNumber = symbolSrcArray[symbolSrcArray.length - 1].split(".")[0];
-      if (symbolNumber === winSymbolCheck && board[i][j].y > 0)
-        winningSymbols.push(board[i][j]);
-    }
+  for (let j = 0; j <= tableDim; j++) {
+    symbolImg = board[0][j].img;
+    symbolSrcArray = symbolImg.src.split("/");
+    symbolNumber = symbolSrcArray[symbolSrcArray.length - 1].split(".")[0];
+    if (symbolNumber === winSymbolCheck && board[0][j].y > 0)
+      winningSymbols.push(board[0][j]);
   }
   if (winningSymbols.length !== 0) animateWin();
 }
 
 function spin() {
+  spriteAnimateCounter = 0;
+  document.body.style.pointerEvents = "none";
   winningSymbols = [];
   currentTime = lastRender = Date.now();
   animateSymbols();
 }
 
-drawTable();
+//drawTable();
 
 addEventListener("load", () => {
   let interval = setInterval(() => {
@@ -215,8 +254,62 @@ addEventListener("load", () => {
 
 addEventListener("keydown", (e) => {
   if (e.code === "Space" && !spaceBlocked) {
+    movementSpeed = canvas.height / 15;
+    getImages(0, false);
     cancelAnimationFrame(spriteAnimateId);
     spaceBlocked = true;
     spin();
   }
 });
+
+// history
+
+/*const historyBtn = document.querySelector(".history-btn");
+const history = document.querySelector(".history");
+const historyScreen = document.querySelector(".history-screen");
+const historyCancel = document.querySelector(".history-cancel");
+const historyScreenImg = historyScreen.querySelector("img");
+const labelRange = document.querySelector(".current-img-label");
+const range = document.querySelector("input[type=range]");
+const rangeBtnPrev = document.querySelector(".range-btn.prev");
+const rangeBtnNext = document.querySelector(".range-btn.next");
+
+range.addEventListener("input", (e) => {
+  currentHistoryImg = e.target.value - 1;
+  historyScreenImg.src = historyImages[currentHistoryImg];
+  labelRange.innerHTML = `${currentHistoryImg + 1}/${historyImages.length}`;
+});
+
+function changePicture(isNext) {
+  if (isNext) {
+    if (currentHistoryImg !== historyImages.length - 1) currentHistoryImg++;
+  } else {
+    if (currentHistoryImg !== 0) currentHistoryImg--;
+  }
+  historyScreenImg.src = historyImages[currentHistoryImg];
+  labelRange.innerHTML = `${currentHistoryImg + 1}/${historyImages.length}`;
+  range.value = currentHistoryImg + 1;
+}
+
+rangeBtnNext.addEventListener("click", () => changePicture(true));
+
+rangeBtnPrev.addEventListener("click", () => changePicture(false));
+
+historyBtn.addEventListener("click", () => {
+  if (historyImages.length > 0) {
+    history.classList.add("active");
+    spaceBlocked = true;
+    currentHistoryImg = 0;
+    historyScreenImg.src = historyImages[currentHistoryImg];
+    range.min = 1;
+    range.max = historyImages.length;
+    range.step = 1;
+    range.value = 1;
+    labelRange.innerHTML = `${currentHistoryImg + 1}/${historyImages.length}`;
+  }
+});
+
+historyCancel.addEventListener("click", () => {
+  history.classList.remove("active");
+  spaceBlocked = false;
+});*/
