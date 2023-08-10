5d1407c7ee0d97860a430d4801b022acf536eed8
NMarko95 <98682412+NMarko95@users.noreply.github.com>
2023-08-10 14:35:37 +0200

    Add files via upload

--- praksa.js
+++ praksa.js
@@ -1,3 +1,25 @@
+// selectors
+const canvas = document.querySelector(".main-canvas");
+const c = canvas.getContext("2d");
+
+const winCanvas = document.querySelector(".win-canvas");
+const winC = winCanvas.getContext("2d");
+
+const gameContainer = document.querySelector(".game");
+const canvasContainer = document.querySelector(".canvas-container");
+const tableHeads = document.querySelectorAll(".th");
+const bankSpan = document.querySelector(".bank");
+const betSpan = document.querySelector(".bet");
+const balanceSpan = document.querySelector(".balance");
+
+const collectBtn = document.querySelector(".collect-btn");
+collectBtn.addEventListener("click", collectMoney);
+
+const tables = document.querySelectorAll(".table");
+
+const scoreContainers = document.querySelectorAll(".score");
+const mainContainer = document.querySelector(".main");
+
 let image, animateId, spriteAnimateId;
 
 let board = [],
@@ -8,15 +30,11 @@ for (let i = 0; i < 5; i++) {
 
 let currentTime, lastRender;
 
-const canvas = document.querySelector(".main-canvas");
-const c = canvas.getContext("2d");
+let tableDim = 5,
+  spriteDim = 260;
 
-const winCanvas = document.querySelector(".win-canvas");
-const winC = winCanvas.getContext("2d");
-
-canvas.height = innerHeight;
-
-let tableDim = 5;
+canvas.height = spriteDim * 5;
+canvas.width = spriteDim;
 
 const cellWidth = canvas.width;
 const cellHeight = canvas.height / 5;
@@ -24,27 +42,34 @@ const cellHeight = canvas.height / 5;
 const symbolHeight = cellHeight / 1.5;
 const symbolWidth = cellWidth / 1.5;
 
-let gameWidth;
-let gameHeight;
+let gameWidth, gameHeight;
 
-let availableSymbols = [3, 5, 7, 8, 10];
+let availableSymbols = [0, 3, 5, 7, 8, 10];
 
 let scores = [
   {
     id: 3,
-    score: 0,
-  },
-  {
-    id: 5,
-    score: 0,
+    multiplier: 2,
+    currentLevel: 5,
+    active: false,
   },
   {
     id: 7,
-    score: 0,
+    multiplier: 1,
+    currentLevel: 5,
+    active: false,
   },
   {
     id: 8,
-    score: 0,
+    multiplier: 1,
+    currentLevel: 5,
+    active: false,
+  },
+  {
+    id: 5,
+    multiplier: 2,
+    currentLevel: 5,
+    active: false,
   },
 ];
 
@@ -54,7 +79,8 @@ function generateRandomNumber() {
 }
 
 let symbols = [],
-  imagesLoaded = false;
+  imagesLoaded = false,
+  backgroundLoaded = false;
 
 function getImages(counter, isSymbols) {
   if (counter === availableSymbols.length) {
@@ -82,25 +108,39 @@ let randomNumber,
   centeredHeight = (cellHeight - symbolHeight) / 2,
   centeredWidth = (cellWidth - symbolWidth) / 2;
 
-function drawImages() {
+function drawImages(isCollect) {
   for (let i = 0; i <= tableDim; i++) {
-    randomSymbol = generateRandomNumber();
-    board[0][i] = {
-      y: (i - 1) * cellHeight + centeredHeight,
-      x: centeredWidth,
-      img: symbols[randomSymbol],
-    };
-    c.drawImage(
-      symbols[randomSymbol],
-      0,
-      0,
-      symbols[randomSymbol].width,
-      symbols[randomSymbol].height,
-      centeredWidth,
-      (i - 1) * cellHeight + centeredHeight,
-      symbolWidth,
-      symbolHeight
-    );
+    if (!isCollect) {
+      randomSymbol = generateRandomNumber();
+      board[0][i] = {
+        y: (i - 1) * cellHeight + centeredHeight,
+        x: centeredWidth,
+        img: symbols[randomSymbol],
+      };
+      c.drawImage(
+        symbols[randomSymbol],
+        0,
+        0,
+        symbols[randomSymbol].width,
+        symbols[randomSymbol].height,
+        centeredWidth,
+        (i - 1) * cellHeight + centeredHeight,
+        symbolWidth,
+        symbolHeight
+      );
+    } else {
+      c.drawImage(
+        board[0][i].img,
+        0,
+        0,
+        board[0][i].img.width,
+        board[0][i].img.height,
+        centeredWidth,
+        (i - 1) * cellHeight + centeredHeight,
+        symbolWidth,
+        symbolHeight
+      );
+    }
   }
 }
 
@@ -109,9 +149,9 @@ let currentImg = generateRandomNumber(),
 
 let animateCounter = 0,
   currentSymbol,
-  movementSpeed = canvas.height / 15,
+  movementSpeed = canvas.height / 20,
   animatesPerSecond = 62,
-  lastAnimate = animatesPerSecond * 3,
+  lastAnimate = animatesPerSecond * 1.5,
   prevFinishedAnimate = lastAnimate / (tableDim - 1),
   spaceBlocked = false;
 
@@ -119,7 +159,7 @@ function drawSymbol(currentSymbol, j) {
   if (currentSymbol.y >= canvas.height) {
     currentImg = generateRandomNumber();
     currentSymbol.img = symbols[currentImg];
-    currentSymbol.y = -cellHeight + centeredHeight / 2;
+    currentSymbol.y = -cellHeight;
   }
   if (animateCounter + 1 === lastAnimate)
     currentSymbol.y = (j - 1) * cellHeight + centeredHeight;
@@ -145,7 +185,7 @@ function animateSymbols() {
     spaceBlocked = false;
     checkWin();
   } else {
-    if (animateCounter === animatesPerSecond * 2.5)
+    if (animateCounter === animatesPerSecond)
       movementSpeed = canvas.height / 60;
     c.clearRect(0, 0, cellWidth, canvas.height);
     for (let j = 0; j <= tableDim; j++) {
@@ -160,7 +200,6 @@ function animateSymbols() {
 let currentSprite,
   spriteAnimateCounter = 0,
   spriteAnimateTime = 40,
-  spriteDim = 260,
   winningSymbol;
 
 winCanvas.height = winCanvas.width = spriteDim;
@@ -187,8 +226,6 @@ function animateWinSymbol() {
   spriteAnimateId = requestAnimationFrame(animateWinSymbol);
 }
 
-const canvasContainer = document.querySelector(".canvas-container");
-
 function makeShadows() {
   c.globalAlpha = 0.5;
   c.clearRect(0, cellHeight * 2, cellWidth, cellHeight);
@@ -210,42 +247,159 @@ function getImageNumber() {
   return symbolSrcArray[symbolSrcArray.length - 1].split(".")[0];
 }
 
-const imageContainers = document.querySelectorAll("img");
-const spanArray = document.querySelectorAll("span");
-
-let correctSpan, symbolImg, symbolSrcArray, symbolNumber;
+let correctSpan,
+  symbolImg,
+  symbolSrcArray,
+  symbolNumber,
+  currentColumnImg,
+  currentTd,
+  currentScoreIndex,
+  audio;
 
 function checkWin() {
+  audio.pause();
   document.body.style.pointerEvents = "auto";
   symbolNumber = getImageNumber();
   currentSprite = sprites.find((sprite) =>
     sprite.src.includes(`${symbolNumber}.png`)
   );
   if (symbolNumber == 10) {
-    spanArray.forEach((span) => (span.innerHTML = "0"));
-    scores.forEach((score) => (score.score = 0));
+    audio = new Audio("./sounds/scatter.mp3");
+    audio.play();
+    scatter();
+  } else if (symbolNumber == 0) {
+    audio = new Audio("./sounds/wild.mp3");
+    audio.play();
+    wild();
   } else {
-    imageContainers.forEach((img) => {
-      symbolSrcArray = img.src.split("/");
-      if (
-        symbolSrcArray[symbolSrcArray.length - 1].includes(`${symbolNumber}`)
-      ) {
-        scores.forEach((score) => {
-          if (score.id === parseInt(symbolNumber)) {
-            correctSpan = img.parentNode.querySelector("span");
-            score.score++;
-            correctSpan.innerHTML = score.score;
+    tableHeads.forEach((th, i) => {
+      currentColumnImg = th.querySelector("img");
+      if (currentColumnImg.src.includes(`${symbolNumber}.png`)) {
+        scores.map((score, i) => {
+          if (
+            score.id == symbolNumber &&
+            score.active &&
+            score.multiplier <= 9
+          ) {
+            score.multiplier += 2;
+            currentScoreIndex = i;
           }
+          if (score.id == symbolNumber) {
+            score.active = true;
+            currentWinning = 0;
+            scores.forEach((score) => {
+              if (score.active) currentWinning += bet * score.multiplier;
+            });
+            balanceSpan.innerHTML = currentWinning;
+          }
+          return score;
         });
+        if (scores[i].currentLevel > 0) {
+          currentTd =
+            th.parentNode.querySelectorAll(".td")[scores[i].currentLevel - 1];
+          currentTd.style.color = "#680ab6";
+          currentTd.style.backgroundColor = "#fff";
+          scores[i].currentLevel -= 1;
+        }
       }
     });
+    audio = new Audio("./sounds/win.mp3");
+    audio.play();
   }
+  checkDisabled();
   animateWin();
 }
 
+let bet = 10,
+  currentBank = 5000,
+  winAmount = 0,
+  currentScore = 0,
+  isDisabled = true,
+  currentWinning = 0,
+  tds;
+
+bankSpan.innerHTML = currentBank;
+betSpan.innerHTML = bet;
+balanceSpan.innerHTML = currentWinning;
+
+function checkDisabled() {
+  scores.forEach((score) => {
+    if (score.active) isDisabled = false;
+  });
+  collectBtn.disabled = isDisabled;
+}
+
+function collectMoney() {
+  audio = new Audio("./sounds/collect.mp3");
+  audio.play();
+  scores.forEach((score) => {
+    if (score.active) {
+      currentScore = bet * score.multiplier;
+    }
+    winAmount += currentScore;
+    currentScore = 0;
+  });
+  currentWinning = 0;
+  balanceSpan.innerHTML = currentWinning;
+  currentBank += winAmount;
+  bankSpan.innerHTML = currentBank;
+  winAmount = 0;
+  scatter();
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  winC.clearRect(0, 0, winCanvas.width, winCanvas.height);
+  cancelAnimationFrame(spriteAnimateId);
+  isDisabled = true;
+  checkDisabled();
+  drawImages(true);
+}
+
+function scatter() {
+  document.querySelectorAll(".td").forEach((td) => {
+    td.style.color = "#fff";
+    td.style.backgroundColor = "#525050";
+  });
+  scores.forEach((score) => {
+    score.currentLevel = 5;
+    score.active = false;
+    if (score.id === 3 || score.id === 5) score.multiplier = 2;
+    else score.multiplier = 1;
+  });
+  currentWinning = 0;
+  balanceSpan.innerHTML = currentWinning;
+}
+
+function wild() {
+  scores.map((score, i) => {
+    if (score.active && score.multiplier <= 9) {
+      score.multiplier += 2;
+      currentScoreIndex = i;
+    }
+    score.active = true;
+    currentWinning = 0;
+    scores.forEach((score) => {
+      if (score.active) currentWinning += bet * score.multiplier;
+    });
+    balanceSpan.innerHTML = currentWinning;
+    return score;
+  });
+  tables.forEach((table, i) => {
+    if (scores[i].currentLevel > 0) {
+      currentTd = table.querySelectorAll(".td")[scores[i].currentLevel - 1];
+      currentTd.style.color = "#680ab6";
+      currentTd.style.backgroundColor = "#fff";
+      scores[i].currentLevel -= 1;
+    }
+  });
+}
+
 function spin() {
+  audio?.pause();
+  audio = new Audio("./sounds/roll.mp3");
+  audio.play();
   getImages(0, false);
   spriteAnimateCounter = 0;
+  currentBank -= bet;
+  bankSpan.innerHTML = currentBank;
   document.body.style.pointerEvents = "none";
   winCanvas.style.display = "none";
   currentTime = lastRender = Date.now();
@@ -258,8 +412,14 @@ function spin() {
   });
 }
 
-const scoreContainers = document.querySelectorAll(".score");
-const mainContainer = document.querySelector(".main");
+const spinBtn = document.querySelector(".spin-btn");
+spinBtn.addEventListener("click", (e) => {
+  e.target.blur();
+  movementSpeed = canvas.height / 20;
+  cancelAnimationFrame(spriteAnimateId);
+  spaceBlocked = true;
+  spin();
+});
 
 let containerHeight, containerWidth;
 
@@ -268,140 +428,75 @@ let aspectRatioScreen = {
   heightScale: 9,
 };
 
-/*function resize(innerWidth, innerHeight) {
+const ASPECT_RATIO = 16 / 9;
+
+function resize() {
   if (innerWidth > innerHeight) {
-    if (
-      gameHeight + 5 >= window.innerHeight &&
-      window.innerWidth >= gameWidth
-    ) {
+    if (onloadScale > ASPECT_RATIO) {
       gameHeight = innerHeight;
-      gameWidth = (gameHeight * 16) / 9;
+      gameWidth =
+        (gameHeight * aspectRatioScreen.widthScale) /
+        aspectRatioScreen.heightScale;
     } else {
       gameWidth = innerWidth;
-      gameHeight = (gameWidth * 9) / 16;
+      gameHeight =
+        (gameWidth * aspectRatioScreen.heightScale) /
+        aspectRatioScreen.widthScale;
     }
   } else {
-    if (
-      gameHeight + 5 >= window.innerHeight &&
-      window.innerWidth >= gameWidth
-    ) {
-      gameWidth = innerWidth;
-      gameHeight = (gameWidth * 16) / 9;
+    if (onloadScale > 1 / ASPECT_RATIO) {
+      gameHeight = outerHeight;
+      gameWidth =
+        (gameHeight * aspectRatioScreen.heightScale) /
+        aspectRatioScreen.widthScale;
     } else {
-      gameHeight = innerHeight;
-      gameWidth = (gameHeight * 9) / 16;
+      gameWidth = outerWidth;
+      gameHeight =
+        (gameWidth * aspectRatioScreen.widthScale) /
+        aspectRatioScreen.heightScale;
     }
   }
 
   resizeElements(gameWidth, gameHeight);
+  proportionalScaleCanvasScore();
 }
 
 function resizeElements(width, height) {
   mainContainer.style.width = width + "px";
   mainContainer.style.height = height + "px";
-}*/
+}
 
 function proportionalScaleCanvasScore() {
-  canvas.style.height = mainContainer.style.height;
-  canvas.style.width = `calc(${mainContainer.style.height} / 5)`;
-  winCanvas.style.height = winCanvas.style.width = canvas.style.width;
-  scoreContainers.forEach((sc) => {
-    sc.style.width = `calc((${mainContainer.style.width} - ${canvas.style.width}) / 2)`;
-  });
+  canvas.style.height = `calc(${mainContainer.style.height} * 0.85)`;
+  canvas.style.width = `calc(${canvas.style.height} / 5)`;
+  winCanvas.style.height =
+    winCanvas.style.width = `calc(${canvas.style.width} / 1.5)`;
+  winCanvas.style.top = `calc(${canvas.style.height} * 0.4 + ${canvas.style.height} / 30)`;
+  winCanvas.style.left = `calc(${winCanvas.style.width} / 4)`;
+  scoreContainers.forEach(
+    (sc) =>
+      (sc.style.width = `calc((${mainContainer.style.width} - ${canvas.style.width}) / 2)`)
+  );
 }
 
 let onloadScale;
-
-function proportionalScale(isLandscape) {
-  containerHeight = parseInt(mainContainer.style.height.split("p")[0]);
-  containerWidth = parseInt(mainContainer.style.width.split("p")[0]);
-  if (isLandscape) {
-    if (
-      containerHeight + 5 >= window.innerHeight &&
-      window.innerWidth >= containerWidth
-    ) {
-      mainContainer.style.height = `${window.innerHeight}px`;
-      mainContainer.style.width = `${
-        (parseFloat(window.innerHeight) * aspectRatioScreen.widthScale) /
-        aspectRatioScreen.heightScale
-      }px`;
-    } else {
-      // slucaj za onload
-      if (mainContainer.style.height === "") {
-        if (onloadScale > 1.7778) {
-          mainContainer.style.height = `${window.innerHeight}px`;
-          mainContainer.style.width = `${
-            (parseFloat(window.innerHeight) * aspectRatioScreen.widthScale) /
-            aspectRatioScreen.heightScale
-          }px`;
-        } else {
-          mainContainer.style.width = `${window.innerWidth}px`;
-          mainContainer.style.height = `${
-            (parseFloat(window.innerWidth) * aspectRatioScreen.heightScale) /
-            aspectRatioScreen.widthScale
-          }px`;
-        }
-      } else {
-        mainContainer.style.width = `${window.innerWidth}px`;
-        mainContainer.style.height = `${
-          (parseFloat(window.innerWidth) * aspectRatioScreen.heightScale) /
-          aspectRatioScreen.widthScale
-        }px`;
-      }
-    }
-  } else {
-    if (
-      containerWidth + 5 >= window.innerWidth &&
-      window.innerHeight >= containerHeight
-    ) {
-      mainContainer.style.width = `${window.innerWidth}px`;
-      mainContainer.style.height = `${
-        (parseFloat(window.innerWidth) * aspectRatioScreen.widthScale) /
-        aspectRatioScreen.heightScale
-      }px`;
-    } else {
-      // slucaj za onload
-      if (mainContainer.style.width === "") {
-        if (onloadScale < 0.5625) {
-          mainContainer.style.width = `${window.outerWidth}px`;
-          mainContainer.style.height = `${
-            (window.outerWidth * aspectRatioScreen.widthScale) /
-            aspectRatioScreen.heightScale
-          }px`;
-        } else {
-          mainContainer.style.height = `${window.innerHeight}px`;
-          mainContainer.style.width = `${
-            (parseFloat(window.innerHeight) * aspectRatioScreen.heightScale) /
-            aspectRatioScreen.widthScale
-          }px`;
-        }
-      } else {
-        mainContainer.style.height = `${window.innerHeight}px`;
-        mainContainer.style.width = `${
-          (parseFloat(window.innerHeight) * aspectRatioScreen.heightScale) /
-          aspectRatioScreen.widthScale
-        }px`;
-      }
-    }
-  }
-  proportionalScaleCanvasScore();
-}
 
 addEventListener("load", () => {
   let interval = setInterval(() => {
     if (imagesLoaded) {
       imagesLoaded = false;
-      drawImages();
+      checkDisabled();
+      drawImages(false);
+      onloadScale = innerWidth / innerHeight;
+      resize();
       clearInterval(interval);
     }
   });
-  onloadScale = innerWidth / innerHeight;
-  proportionalScale(window.innerWidth > window.innerHeight);
 });
 
 addEventListener("keydown", (e) => {
   if (e.code === "Space" && !spaceBlocked) {
-    movementSpeed = canvas.height / 15;
+    movementSpeed = canvas.height / 20;
     cancelAnimationFrame(spriteAnimateId);
     spaceBlocked = true;
     spin();
@@ -409,6 +504,6 @@ addEventListener("keydown", (e) => {
 });
 
 window.onresize = () => {
-  console.log("resized");
-  proportionalScale(window.innerWidth > window.innerHeight);
+  onloadScale = innerWidth / innerHeight;
+  resize();
 };
